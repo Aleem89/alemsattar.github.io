fb8bad7b5fad85744b884f26b2c9f03a9baf2b6a
Aleem89 <156438218+Aleem89@users.noreply.github.com>
2024-03-17 01:03:33 -0500

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -51,4 +51,27 @@ window.addEventListener('DOMContentLoaded', event => {
         });
     });
 
+    //Counter 
+    const decreaseBtn = document.getElementById("decreaseBtn");
+    const resetBtn = document.getElementById("resetBtn");
+    const increaseBtn = document.getElementById("increaseBtn");
+    const countLabel = document.getElementById("countLabel");
+    let count = 0;
+
+    increaseBtn.onclick = function(){
+        count++;
+        countLabel.textContent = count;
+    }
+
+    decreaseBtn.onclick = function(){
+        count--;
+        countLabel.textContent = count;
+    }
+
+    resetBtn.onclick = function(){
+        count = 0;
+        countLabel.textContent = count;
+    }
+
+
 });
